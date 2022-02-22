24a62213213a0bcf9bece9c8719a61129ebfd78c
BohuaHuang <100243902+Bh0919@users.noreply.github.com>
2022-02-22 15:57:04 -0800

    Update Map.py

--- Map.py
+++ Map.py
@@ -6,7 +6,7 @@ floor = mpimg.imread('floor.jpg', format = "jpg").copy()
 Map = mpimg.imread('blank.jpg', format = "jpg").copy()
 fig, ax = plt.subplots(1)
 
-len_floor, wid_floor = floor.shape
+len_floor, wid_floor, pixels = floor.shape
 for i in range(14):
     for j in range(14):
         Map[i*len_floor:(i+1)*len_floor,j*wid_floor:(j+1)*wid_floor] = floor
