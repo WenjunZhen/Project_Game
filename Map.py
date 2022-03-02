9eb290391c581178036257b05d59f4091ecf5b22
BohuaHuang <100243902+Bh0919@users.noreply.github.com>
2022-03-01 16:47:05 -0800

    Update Map.py

--- Map.py
+++ Map.py
@@ -5,24 +5,58 @@ class Map:
     def __init__(self):
         self.hw = mpimg.imread('hw.jpg', format = "jpg").copy()
         self.vw = mpimg.imread('vw.jpg', format = "jpg").copy()
+        self.darkness = mpimg.imread('darkness.jpg', format = "jpg").copy()
         self.floor = mpimg.imread('floor.jpg', format = "jpg").copy()
         self.Map = mpimg.imread('blank.jpg', format = "jpg").copy()
         self.len_floor, self.wid_floor, self.pixels_floor = self.floor.shape
         self.len_hw, self.wid_hw, self.pixels_hw = self.hw.shape
         self.len_vw, self.wid_vw, self.pixels_vw = self.vw.shape
+        visiableIndex = {}
+        wallIndex = []
+        
         for i in range(24):
             for j in range(24):
                 self.Map[i*self.len_floor:(i+1)*self.len_floor,j*self.wid_floor:(j+1)*self.wid_floor] = self.floor
+                visiableIndex[(i,j)] = False
+                
+        for i in range(2):
+            self.Map[(0)*self.len_hw+600:(1)*self.len_hw+600,(3*i)*self.wid_hw+400:(3*i+1)*self.wid_hw+400] = self.hw
+            self.Map[(0)*self.len_hw+4200:(1)*self.len_hw+4200,(3*i)*self.wid_hw+400:(3*i+1)*self.wid_hw+400] = self.hw
+            
+        self.Map[(0)*self.len_vw+800:(1)*self.len_vw+800,(0)*self.wid_vw+200:(1)*self.wid_vw+200] = self.vw
+        self.Map[(0)*self.len_vw+800:(1)*self.len_vw+800,(0)*self.wid_vw+4400:(1)*self.wid_vw+4400] = self.vw
+        self.Map[(0)*self.len_vw+3200:(1)*self.len_vw+3200,(0)*self.wid_vw+200:(1)*self.wid_vw+200] = self.vw
+        self.Map[(0)*self.len_vw+3200:(1)*self.len_vw+3200,(0)*self.wid_vw+4400:(1)*self.wid_vw+4400] = self.vw
+        
+        for i in range(2):
+            self.Map[(0)*self.len_hw+1600:(1)*self.len_hw+1600,(2*i)*self.wid_hw+1000:(2*i+1)*self.wid_hw+1000] = self.hw
+            self.Map[(0)*self.len_hw+3200:(1)*self.len_hw+3200,(2*i)*self.wid_hw+1000:(2*i+1)*self.wid_hw+1000] = self.hw
+            
+        self.Map[(0)*self.len_vw+2000:(1)*self.len_vw+2000,(0)*self.wid_vw+600:(1)*self.wid_vw+600] = self.vw
+        self.Map[(0)*self.len_vw+2000:(1)*self.len_vw+2000,(0)*self.wid_vw+4000:(1)*self.wid_vw+4000] = self.vw
+        
         for i in range(3):
-            self.Map[(5*i+2)*self.len_hw:(5*i+3)*self.len_hw,(i+1)*self.wid_hw:(i+2)*self.wid_hw] = self.hw
-        for i in range(3):
-            self.Map[(i)*self.len_vw+600:(i+1)*self.len_vw+600,(5*i+4)*self.wid_vw:(5*i+5)*self.wid_vw] = self.vw
-        for i in range(3,6):
-            self.Map[(4*i+2)*self.len_hw:(4*i+3)*self.len_hw,1*self.wid_hw-200:2*self.wid_hw-200] = self.hw
+            self.Map[(i)*self.len_vw+800+200*i:(i+1)*self.len_vw+800+200*i,(0)*self.wid_vw+2400:(1)*self.wid_vw+2400] = self.vw
+            
+            
+        self.realMap = self.Map
+        
+        #for i in range(24):
+            #for j in range(24):
+                #self.Map[i*self.len_floor:(i+1)*self.len_floor,j*self.wid_floor:(j+1)*self.wid_floor] = self.darkness
 
     def printMap(self):
         fig, ax = plt.subplots(1)
         plt.imshow(self.Map)
+        
+    def updateVisiableMap(self):
+        for i in range(24):
+            for j in range(24):
+                if visiableIndex[(i,j)]:
+                    self.Map[i*self.len_floor:(i+1)*self.len_floor,
+                             j*self.wid_floor:(j+1)*self.wid_floor] = self.realMap[i*self.len_floor:(i+1)*self.len_floor,
+                                                                                   j*self.wid_floor:(j+1)*self.wid_floor]
+
         
 m = Map()
 m.printMap()
