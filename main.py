6bee263cf40b58ece11401dea076dbd9eb70fd31
DavidHe0802 <97209947+DavidHe0802@users.noreply.github.com>
2022-03-08 12:35:34 -0800

    Update Encounter function and organzied codes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+######################### Implementing Main Function ################################
+
+
 def moving(m,c):
     Game_Over = False
     
@@ -19,38 +22,89 @@ def moving(m,c):
 
         m.Create_character(c)
 
-            
+
+
 def encounter(character, Monster):    
+        if Monster.x==23 and Monster.y==23:
+            print("congraduation! You win the game! Cheers!")
+            print("""☆ *　. 　☆
+　　. ∧＿∧　∩　* ☆
+* ☆ ( ・∀・)/ .
+　. ⊂　　 ノ* ☆
+☆ * (つ ノ .☆
+　　 (ノ""")
+            return
+        
         if Monster.HP>0:
             print("You encountered a monster! Now let's fight!")
     
-        while Monster.HP>0 or character.HP>0:
-            Monster.BeAttacked(10)
-        # player be attacked by enenmy
+        while Monster.HP>0 and character.HP>0:
+            Monster.BeAttacked(character.attack)
+            character.HP=character.HP - Monster.attack
+            print("Monster attacked you! You now have " + str(character.HP) + " hit points!")
+            print("You attacked the monster! It now has " + str(Monster.HP) + " hit points!")
         
-            if Monster.HP>0:
-                print("So bad, you died! The Monster still has " + str(Monster.HP) + " more hit points!")
-                return    
+        if Monster.HP>0:
+            print("So bad, you died! The Monster still has " + str(Monster.HP) + " more hit points!")
+            return    
     
-            if Monster.HP<=0:
-                answer = input("You found a Goodie! Select from 1) Torch, 2) Archor, 3) Armor.")
-                if answer == "1":
-                    print("Now you have a torch in your hand, congratulations!")
-                    return
-                elif answer == "2":
-                    print("Now you have a archor in your hand, congratulations!")
-                    return
-                elif answer == "3":
-                    print("Now you have a armor in your hand, congratulations!")
-                    return
-                else:
-                    print("It's fine if you don't need anything.")
-                    return
-                  
+        if Monster.HP<=0:
+            answer = input("You found a Goodie! Select from 1) Torch, 2) Archor, 3) Armor.")
+            if answer == "1":
+                print("Now you have a torch in your hand, congratulations!")
+                print("Currently its a useless tool, but you looks fancier!")
+                return
+            elif answer == "2":
+                print("Now you have a archor in your hand, congratulations!")
+                character.attack = character.attack + 10
+                print("Your Attacks increased by 10!")
+                return
+            elif answer == "3":
+                print("Now you have a armor in your hand, congratulations!")
+
+                character.HP = character.HP + 50
+                print("Your Hitpoints increased by 50!")
+                return
+            else:
+                print("It's fine, you don't have to select a goodie. It's not a bad idea to rely only on yourself!")
+                return
+        
+        
+        
+def CheckEncounter(Monsterlist,character):
+    for i in range(len(Monsterlist)):
+        if Monsterlist[i].x==character.x and Monsterlist[i].y==character.y:
+            encounter(character,Monsterlist[i])
+            break
+
+            
+            ########################## item Creation ##############################
 m = Map()
-w = weapon("name",10)
-c = character("hello",w)
-c.show_status()
+
+Weapon = weapon("Test Weapon",10)
+Character = character("Tester",Weapon)
+
+EndPoint = Monster(23,23,"End Point",0)
+m.Create_Monster(EndPoint)
+
+Monster1 = Monster(1,6,"Monster1",1)
+m.Create_Monster(Monster1)
+
+Monster2 = Monster(10,7,"Monster2",1)
+m.Create_Monster(Monster2)
+
+Monster3 = Monster(15,5,"Monster3",1)
+m.Create_Monster(Monster3)
+
+MonsterList=[Monster1, Monster2, Monster3, EndPoint]
+
+
+
+######################## Game Interface #########################
+
+Character.show_status()
+moving(m,Character)
+m.Create_character(Character)
+CheckEncounter(MonsterList,Character)
+
 m.printMap()
-moving(m,c)
-m.Create_character(c)
