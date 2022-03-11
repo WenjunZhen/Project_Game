63aaccbff06e6132de16dff5283d52dfba3e1933
WenjunZhen <97010619+WenjunZhen@users.noreply.github.com>
2022-03-11 00:22:09 -0800

    Update MonsterAndGoodie.py

--- MonsterAndGoodie.py
+++ MonsterAndGoodie.py
@@ -3,11 +3,26 @@
 ############################## MONSTER CLASS ######################################
 
 class Monster:
-
+    '''
+    A class represent the monster, got some attack, HP and position
+    '''
     def __init__(self, x_col, y_col, name, HP = 100, attack = 3):
+        '''
+        Initialize the name, read image:
+        1.pic_monster: The one character going to kill
+        2.pic_goodie: The thing character going to get from killing the monster
+        3.pic_flag: end-point
+        4.boss: final boss to kill
+        Set the x, y, attack, and name for the monster
+        Args:
+            None
+        Return: 
+            None
+        '''
         self.pic_monster = mpimg.imread('pic_monster.jpg', format = "jpg").copy()
         self.pic_goodie = mpimg.imread('pic_goodie.jpg', format = "jpg").copy()
         self.pic_flag = mpimg.imread('pic_flag.jpg', format = "jpg").copy()
+        self.boss = mpimg.imread('boss.jpg', format = "jpg").copy()
         self.len_pic_monster, self.wid_pic_monster, self.pixels_pic_goodie = self.pic_monster.shape
         self.len_pic_goodie, self.wid_pic_goodie, self.pixels_pic_goodie = self.pic_goodie.shape
         self.x = x_col
@@ -17,8 +32,18 @@ class Monster:
         self.name = name
     
     def pic(self):
+    '''
+    If the position x and y equal to 23 and if the HP less than 0,return pic_flasg or boss, else return pic_goodie or pic_monster
+    Args:
+         Set up and if statement and make one more if statement inside to determine the conditions satifie or not.
+    Return: 
+         return pic_flasg or boss, else return pic_goodie or pic_monster
+    '''
         if self.x==23 and self.y==23:
-            return self.pic_flag
+            if self.HP<=0:
+                return self.pic_flag
+            else:
+                return self.boss
         else:
             if self.HP<=0:
                 return self.pic_goodie
@@ -26,33 +51,89 @@ class Monster:
                 return self.pic_monster
     
     def length(self):
+    '''
+    set up a condition to determine return goodie or monster for length
+    Args:
+         Make an if statement
+    Return: 
+         self.len_pic_goodie or self.len_pic_monster
+    '''
         if self.HP<=0:
             return self.len_pic_goodie
         else:
             return self.len_pic_monster
         
     def width(self):
+    '''
+    set up a condition to determine return goodie or monster for width
+    Args:
+         Make an if statement
+    Return: 
+         self.len_pic_goodie or self.len_pic_monster
+    '''
         if self.HP<=0:
             return self.wid_pic_goodie
         else:
             return self.wid_pic_monster
     
     def attack(self):
+    '''
+    set up monster attack
+    Args:
+         None
+    Return: 
+           self.attack
+    '''
         return self.attack
     
     def BeAttacked(self,AttackFromCharacter):
+    '''
+    Getting attack from character
+    Args:
+         Subtrack the HP from the attack damage
+    Return: 
+         None
+    '''
         self.HP = self.HP - AttackFromCharacter
     
     def HP(self):
+    '''
+    Set up HP
+    Args:
+         None
+    Return: 
+           HP
+    '''
         return self.HP
     
     def name(self):
+    '''
+    set up name
+    Args:
+         None
+    Return: 
+           name
+    '''
         return self.name
     
     def x(self):
+    '''
+    set up x position
+    Args:
+         None
+    Return: 
+           self.x
+    '''
         return self.x
     
     def y(self):
+    '''
+    set up y position
+    Args:
+         None
+    Return: 
+           self.y
+    '''
         return self.y
 
 ##################################################################################
@@ -69,7 +150,15 @@ class Map:
 ################################################################################
 ############################## GENERAL FUNCTIONS ########################################
 
-def encounter(character, Monster):    
+def encounter(character, Monster):
+    '''
+    If the character meet the monster, the follwing condition will occur if it satifie the condition
+    Args:
+         Make multiple of if statement to check if the conditions satifie and a while loop to subtract the HP from character and monster
+    Return: 
+         empty
+    '''
+    
         if Monster.x==23 and Monster.y==23:
             print("congraduation! You win the game! Cheers!")
             print("""☆ *　. 　☆
@@ -115,6 +204,13 @@ def encounter(character, Monster):
                 return
                   
 def CheckEncounter(Monsterlist,character):
+    '''
+    To check if monster and character are on the same location
+    Args:
+         apply the encounter function if the location are the same
+    Return: 
+         None
+    '''
     for i in range(len(Monsterlist)):
         if Monsterlist[i].x==character.x and Monsterlist[i].y==character.y:
             encounter(character,Monsterlist[i])
