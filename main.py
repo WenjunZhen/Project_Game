0b3322ece6ec9920d327c62e1e7da690cf049c14
BohuaHuang <100243902+Bh0919@users.noreply.github.com>
2022-03-10 21:43:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from function import moving, encounter, CheckEncounter, MonsterGenerator, game
 m = Map()
 Weapon = weapon("Weapon",10)
 Character = character("Tester",Weapon)
-
+m.Create_character(Character)
 EndPoint = Monster(23,23,"End Point",100,10)
 m.Create_Monster(EndPoint)
 MonsterIndex=[(23,23)]
