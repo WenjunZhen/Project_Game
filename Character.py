87afbbe616644ad6b4a6672e849594f4d6911f67
WenjunZhen <97010619+WenjunZhen@users.noreply.github.com>
2022-03-10 21:50:13 -0800

    Update Character.py

--- Character.py
+++ Character.py
@@ -1,9 +1,30 @@
 class weapon:
+    '''
+    A class represent weapon,getting weapon from killing the monster
+    '''
     def __init__(self,weapon_name,strength):
+        '''
+        Initialize the the weapon
+        Given the name of the weapon and it's strength
+        Args:
+            None
+        Return: 
+            None
+        '''
         self.weapon_name = weapon
         self.strength = strength
 class character:
+    '''
+    A class represent the character, shows the status of the character
+    '''
     def __init__(self, name, weapon, HP = 100,attack = 5):
+        '''
+        Initialize the name, read image, HP, attack,x and y of the character
+        Args:
+            None
+        Return: 
+            None
+        '''
         self.name = name
         try:
         self.pic_character = mpimg.imread('character.jpg', format = "jpg").copy()
@@ -12,29 +33,46 @@ class character:
         self.weapon = weapon
         self.HP = HP
         self.attack = attack
+        self.x = x_col
+        self.y=y_col
     def show_status(self):
+        '''
+        Show the status of the character
+        Args:
+            Print out the attack and HP
+        Return: 
+            None
+        '''
         print("Your Status")
         print("Attack = ",self.attack)
         print("HP = ", self.HP)
     def __del__(self):
+        '''
+        The character is death
+        Args:
+            None
+        Return: 
+            name and you died
+        '''
         return(self.name," You Died!")
  
-class Map:
-    def Create_character(self,character):
-        self.Map[character.x*character.length():(character.x + 1)*character.length(),
-                 character.y*character.width():(character.y + 1)*character.width()] = character.pic() 
 
-Game_Over = False 
-while (Game_Over == False):
-    answer = input("Please enter a movement W,A,S,D: ")
-    if answer == "W":
-        y+=1
-    elif answer == "A":
-        x-=1
-    elif answer == "S":
-        y-=1
-    elif answer == "D":
-        x+=1
+def moving(m,c):
+    
+    m.Clean_character_position(c)
+    answer = input("Please enter a movement W,A,S,D,E: ")
+    if m.check_character_position(c, answer):
+        if answer == "W":
+             c.x-=1
+        elif answer == "A":
+            c.y-=1
+        elif answer == "S":
+            c.x+=1
+        elif answer == "D":
+             c.y+=1
+
+        m.Create_character(c)
+        return answer
         
 ################################################# Monster Generator        
 for i in range(30):
